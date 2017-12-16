b9bf2f10fa30bf0486a3699e780ec22d0fd7ff74
Reid 'arrdem' McKenzie <me@arrdem.com>
2017-12-15 17:54:20 -0800

    Eval status-to-version

--- src/lein_git_version/plugin.clj
+++ src/lein_git_version/plugin.clj
@@ -12,7 +12,15 @@
     project-version :version
     :as             project}]
   (let [{:keys [path root-ns set-version status-to-version] :as config} (merge default-config git-version)        
-        {:keys [tag version ahead ahead? ref ref-short] :as status}     (git-status config)]
+        {:keys [tag version ahead ahead? ref ref-short] :as status}     (git-status config)
+        status                                                          (dissoc status :version)
+        
+        status-to-version (when status-to-version
+                            (try (eval status-to-version)
+                                 (catch Exception e
+                                   (printf "While trying to evaluate status-to-version function:\n%s\n\nEncountered exception:\n%s\n"
+                                           status-to-version e)
+                                   (System/exit 1))))]
     (cond-> (merge project status)
       (or (= set-version :git-ref-short)
           (= project-version :project/git-ref-short))
