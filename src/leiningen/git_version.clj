9db616a8cd9d952c2067edef5f6f49beb81385ee
Reid 'arrdem' McKenzie <me@arrdem.com>
2017-12-15 19:21:40 -0800

    Overhaul git-describe, add errors & make pattern customizable

--- src/leiningen/git_version.clj
+++ src/leiningen/git_version.clj
@@ -3,12 +3,13 @@
   (:require [clojure.string :as str]
             [clojure.java.shell :refer [sh]]))
 
-(def default-config
-  {:git            "git"
-   :tag-to-version nil})
+(def git-describe-pattern
+  #"(?<tag>.*)-(?<ahead>\d+)-g(?<ref>[0-9a-f]*)(?<dirty>(-dirty)?)")
 
-(def git-dirty-pattern
-  #"(?<tag>.*)-(?<ahead>\d+)-g(?<ref>[0-9a-f]*)(?<snapshot>(-dirty)?)")
+(def default-config
+  {:git              "git"
+   :describe-pattern git-describe-pattern
+   :tag-to-version   nil})
 
 (defn get-git-ref
   ""
@@ -28,18 +29,56 @@
   (let [cmd [git "log" "-1" "--pretty=%ct" ref-or-sha]]
     (str/trim (:out (apply sh cmd)))))
 
+(defmacro let-groups
+  "Let for binding groups out of a j.u.Pattern j.u.r.Matcher."
+  {:style/indent [2]}
+  [m bindings & body]
+  (let [s (with-meta (gensym "matcher") {:tag java.util.regex.Matcher})]
+    `(let [~s ~m
+           ~@(mapcat identity
+                     (for [b bindings]
+                       `[~b (.group ~s ~(name b))]))]
+       ~@body)))
+
+(defn ensure-pattern [x]
+  (if (string? x)
+    (re-pattern x)
+    x))
+
+(defn parse-git-describe
+  [{:keys [describe-pattern] :as config} out]
+  (let [pattern (ensure-pattern describe-pattern)
+        matcher (re-matcher pattern out)]
+    (if-not (.matches matcher)
+      (do (binding [*out* *err*]
+            (printf "Warning: lein-git-version didn't match the current repo status:\n%s\n\nAgainst pattern:\n%s\n"
+                    (pr-str out) pattern)
+            (.flush *out*))
+          {})
+      (let-groups matcher [tag ahead ref dirty]
+                  {:tag       tag
+                   :ahead     (Integer/parseInt ahead)
+                   :ahead?    (not= ahead "0")
+                   :ref       (get-git-ref config)
+                   :ref-short ref
+                   :dirty?    (not= "" dirty)}))))
+
+(defn git-describe
+  ""
+  [{:keys [git] :as config}]
+  (let [{:keys [exit out] :as child} (apply sh [git "describe" "--tags" "--dirty" "--long"])]
+    (if-not (= exit 0)
+      (binding [*out* *err*]
+        (printf "Warning: lein-git-version git exited %d\n%s\n"
+                exit child)
+        (.flush *out*)
+        {})
+      (parse-git-describe config (str/trim out)))))
+
 (defn git-status
   "Fetch the current git status."
-  [{:keys [git] :as config}]
-  (let [buff                       (:out (apply sh [git "describe" "--tags" "--dirty" "--long"])) 
-        [_ tag ahead ref dirty? _] (or (re-find git-dirty-pattern buff)
-                                       [nil nil nil true])
-        dirty?                     (not= "" dirty?)]
-    (cond-> {:tag       tag
-             :ahead     (Integer/parseInt ahead)
-             :ahead?    (not= ahead "0")
-             :ref       (get-git-ref config)
-             :ref-short ref}
-      dirty?       (assoc :dirty? true)
+  [config]
+  (if-let [{:keys [dirty?] :as status} (git-describe config)]
+    (cond->  status
       (not dirty?) (assoc :message (git-ref-message config "HEAD"))
       (not dirty?) (assoc :timestamp (git-ref-ts config "HEAD")))))
